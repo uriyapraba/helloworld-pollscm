4c79e476fbf70d30d3d5aa4fef5f03460e6e5fe3
Indrajith <vboxuser@ubuntu.myguest.virtualbox.org>
2023-07-25 09:31:39 +0530

    changes made in Helloworld script

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld
 {
 	public static void main(String[] args)
 	{
-		System.out.println("******Hello World & welcome to Jenkins Training");
+		System.out.println("******Made changes in the repo******");
 	}
 }
