4903411840004605afa150b4df2d7e7c0cbb85ad
uriyapraba <136190350+uriyapraba@users.noreply.github.com>
2023-07-25 11:15:31 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld
 {
 	public static void main(String[] args)
 	{
-		System.out.println("******Welcome to Webhooks******");
+		System.out.println("******Welcome to Webhooks********");
 	}
 }
